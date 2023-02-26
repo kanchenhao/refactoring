1cc3358ece664750be3ebe61742c8eb89e2e79bc
kanchenhao <azstaylorswift13@gmail.com>
2023-02-26 14:21:32 +0800

    chapter01: 打印订单详情, 改变函数声明，移除play参数

--- src/chapter01/statement.js
+++ src/chapter01/statement.js
@@ -1,4 +1,28 @@
 function statement(invoice, plays) {
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+
+    return result;
+  }
+
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
   }
@@ -10,7 +34,7 @@ function statement(invoice, plays) {
     { style: 'currency', currency: 'USD',
       minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-    const thisAmount = amountFor(perf, playFor(perf));
+    const thisAmount = amountFor(perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -22,29 +46,6 @@ function statement(invoice, plays) {
   }
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
-  return result;
-}
-
-function amountFor(aPerformance, play) {
-  let result = 0;
-  switch (play.type) {
-    case 'tragedy':
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case 'comedy':
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-  }
-
   return result;
 }
 
